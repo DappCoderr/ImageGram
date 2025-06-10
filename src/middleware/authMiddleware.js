92b137c18b90e04507c1b0542283596335015ef8
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-06-10 19:04:59 +0530

    add middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import { verifyJwt } from "../utils/jwt";
+import { checkIfUserExists } from "../services/userService.js";
+
+export const isAuthenticated = async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(400).json({
+      success: false,
+      message: "Token is required",
+    });
+  }
+
+  try {
+    const response = verifyJwt(token);
+    const doesUserExist = await checkIfUserExists(response.email);
+    if (!doesUserExist) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    req.user = response;
+    next();
+  } catch (error) {}
+};
